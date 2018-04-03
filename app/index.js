3811914051a47f84224ec424d8f0d6ae264084fc
marb61a <marb61a@gmail.com>
2018-04-03 16:26:35 +0100

    added initial files and installed react and react-dom

---
+++ app/index.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        Hello World
+      </div>
+    );
+  }
+}
